3384ed29ea3ae866a59b75478ce1a093add8bd06
SabbirSami <smd71430@gmail.com>
2022-03-24 17:01:00 +0600

    hero section done

--- src/components/Hero-Section/Hero.js
+++ src/components/Hero-Section/Hero.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import "./Hero.css";
+import heroImg from "../../images/Group 12.jpg";
 
 const Hero = () => {
     return (
         <div>
             <Container>
-                <Row>
-                    <Col>1 of 2</Col>
-                    <Col>2 of 2</Col>
-                </Row>
-                <Row>
-                    <Col>1 of 3</Col>
-                    <Col>2 of 3</Col>
-                    <Col>3 of 3</Col>
-                </Row>
+                <div className="hero-section">
+                    <Row>
+                        <Col>
+                            <div className="hero-text-area">
+                                <p className="text-warning">
+                                    Sale up to 70% off
+                                </p>
+                                <h1>New Collection For Fall</h1>
+                                <h4>
+                                    Discover all the new arrivals of
+                                    ready-to-wear collection.
+                                </h4>
+                                <button className="btn btn-warning py-2 px-4 fs-5">
+                                    Shop Now
+                                </button>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="hero-img-area justify-content-center">
+                                <img src={heroImg} alt="" />
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </Container>
         </div>
     );
